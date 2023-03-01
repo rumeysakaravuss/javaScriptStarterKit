6955b42835fcac8c5ae9434ccdf5dc3a8845bfb9
Rümeysa <rumeysakaravus05@gmail.com>
2023-03-01 16:20:14 +0300

    javaScript 1. ders

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-console.log("Merhaba Kodlama.io")
+console.log("merhaba kodlama.io")
 
-//JS type safe değildir
+//JS type safe değildir.
 let dolarBugun = 9.30
 
 let dolarDun = 9.20
 dolarDun = "9.20"
+
 {
     let dolarDun = 9.10
 }
@@ -12,25 +13,22 @@ dolarDun = "9.20"
 console.log(dolarDun)
 
 const euroDun = 11.2
-//euroDun = 11
+//euroDun = 11 bunu yapamıyoruz//
 
 console.log(euroDun)
 
 //array
 //camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+//pascalCasing
+let konutKredileri  = ["konut kredisi","emlak konut kredisi","kamu konut kredisi"]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for (let i = 0;i<3;i++){
+     console.log("<li>"+konutKredileri[i]+"</li>")
 }
-console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
+
+console.log("<ul>")
+
 
 console.log(konutKredileri)
