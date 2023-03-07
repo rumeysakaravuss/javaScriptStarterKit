777e0cac18acf97c3efd599e22573537b5750bac
Rümeysa <114934233+rumeysakaravuss@users.noreply.github.com>
2023-03-07 16:36:12 +0300

    Add files via upload

--- src/functions.js
+++ src/functions.js
@@ -41,3 +41,63 @@ let product3 ={productName:"elma",unitPrice:10,quantity:5}
 product2 = product3
 product2.productName = "KARPUZ"
 console.log(product3.productName)
+
+let sayi1 = 10 
+let sayi2 = 20 
+sayi1 = sayi2
+sayi2 = 100 
+console.log(sayi1)
+
+function addToCart4 (x){
+    console.log(products)
+}
+
+let products = [
+    {productName:"Elma", unitPrice:10,quantity:5},
+    {productName:"Armut", unitPrice:10,quantity:5},
+    {productName:"Karpuz", unitPrice:10,quantity:5}
+]
+
+addToCart4(products)
+
+function add(bisey,...numbers){ //rest
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total = total + numbers[i]
+    }
+    console.log(total)
+    console.log(bisey)
+}
+
+
+add(20,30)
+//add(20,30,40)
+//add(20,30,40,50)
+
+let numbers = [30,10,500,600,120]
+//console.log(...numbers)
+console.log(Math.max(...numbers))
+
+let [icAnadolu,Marmara,Karadeniz,[İcAnadoluSehirleri]] = [
+    {name:"İç Anadolu", population:"20M"},
+    {name:"Marmara", population:"30M"},
+    {name:"Karadeniz", population:"10M"},
+    [
+        ["Ankara","Konya"],
+        ["İstanbul","Bursa"],
+        ["Sinap","Trabzon"],
+    ]
+]
+
+//console.log(icAnadolu.population)
+//console.log(icAnadolu.name)
+console.log(İcAnadoluSehirleri)
+
+let newProductName, newUnitPrice, newQuantity
+({productName:newProductName,unitPrice:newUnitPrice,quantity:newQuantity} 
+= {productName:"Elma", unitPrice:10,quantity:5})
+
+console.log(newProductName)
+console.log(newUnitPrice)
+console.log(newQuantity)
+
